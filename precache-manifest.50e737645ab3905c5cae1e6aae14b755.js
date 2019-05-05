3f49309b6e7241fa2bf0756ee526701638710e2a
Peter Tag <peter.h.tag@gmail.com>
2019-05-05 10:11:25 -0700

    Updates

---
+++ precache-manifest.50e737645ab3905c5cae1e6aae14b755.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9191433aaf89f8351c62bce20bd5ecbb",
+    "url": "/clicky-the-game/index.html"
+  },
+  {
+    "revision": "b8cec2d1664a71f4c737",
+    "url": "/clicky-the-game/static/css/main.49acdfb6.chunk.css"
+  },
+  {
+    "revision": "ff2b54d40a6d618c8c20",
+    "url": "/clicky-the-game/static/js/2.5996a945.chunk.js"
+  },
+  {
+    "revision": "b8cec2d1664a71f4c737",
+    "url": "/clicky-the-game/static/js/main.8994f1de.chunk.js"
+  },
+  {
+    "revision": "a91431e535c3f9315232",
+    "url": "/clicky-the-game/static/js/runtime~main.f53ede3d.js"
+  }
+]);
