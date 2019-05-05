ca545dd13f538140cedf9ea84355653b146bbacf
Peter Tag <peter.h.tag@gmail.com>
2019-05-04 20:28:25 -0700

    Updates with click event handler in cards

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,13 +14,25 @@ class App extends React.Component {
       topScore: 0,
       selected: []
     };
+    componentDidMount () {
+      this.state.myImages.forEach(element => {
+        this.state.selected.push(false);
+      });
+      alert("Did mount. length" +  this.state.myImages.length);
+    }
   
     handleImageClickEvent = id => {
       // Filter this.state.friends for friends with an id not equal to the id being removed
       // const friends = this.state.friends.filter(friend => friend.id !== id);
       // // Set this.state.friends equal to the new friends array
       // this.setState({ images });
-      alert("Image click for id=" + id);
+      let selected = [...this.state.selected];
+      let index = Number(id);
+
+      alert("Image click for id=" + id + " selected now=" + selected[index]);
+      selected[index] = true;
+
+      this.setState({selected});
     };
 
 render() {
